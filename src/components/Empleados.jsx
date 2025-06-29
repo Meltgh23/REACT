9408fdb38bd0faa1504057a14baa06b80bcb6199
MelvinMeltgh23 <melvingutierrez3302@gmail.com>
2025-06-29 17:47:32 -0600

    actualizar 3

--- src/components/Empleados.jsx
+++ src/components/Empleados.jsx
@@ -7,22 +7,38 @@ import '../index.css';
 
 function Empleado() {//aqui son los estodos 
     const [nombre, setNombre] = useState("");
+    const [apellidopaterno, setApellidopaterno] = useState("");
+    const [apellidomaterno, setApellidomaterno] = useState("");
   const [puesto, setPuesto] = useState("");
   const [correo, setCorreo] = useState("");
+  const [numero, setNumero] = useState("");
+  const [nss, setNss] = useState("");
+  const [rfc,setRfc] = useState("");
   const [empleados, setEmpleados] = useState([]);
-//funcion para guardar empleados
+//aky guardo empleados
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await addDoc(collection(db, "empleados"), {
         nombre,
+        apellidopaterno,
+        apellidomaterno,
         puesto,
         correo,
+        numero,
+        nss,
+        rfc,
+        
       });
       alert("Empleado guardado correctamente");
       setNombre("");
+      setApellidopaterno("");
+      setApellidomaterno("");
       setPuesto("");
       setCorreo("");
+      setNumero("");
+      setNss("");
+      setRfc("");
     } catch (error) {
       console.error("Error al guardar empleado:", error);
       alert("Hubo un error al guardar");
@@ -52,6 +68,20 @@ useEffect(() => {
           onChange={(e) => setNombre(e.target.value)}
           required
         />
+          <input
+          type="text"
+          placeholder="Apellido Paterno"
+          value={apellidopaterno}
+          onChange={(e) => setApellidopaterno(e.target.value)}
+          required
+        />
+          <input
+          type="text"
+          placeholder="Apellido Materno"
+          value={apellidomaterno}
+          onChange={(e) => setApellidomaterno(e.target.value)}
+          required
+        />
         <input
           type="text"
           placeholder="Puesto"
@@ -64,6 +94,27 @@ useEffect(() => {
           placeholder="Correo electrónico"
           value={correo}
           onChange={(e) => setCorreo(e.target.value)}
+          required
+        />
+          <input
+          type="number"
+          placeholder="Numero"
+          value={numero}
+          onChange={(e) => setNumero(e.target.value)}
+          required
+        />
+        <input
+          type="number"
+          placeholder="NSS"
+          value={nss}
+          onChange={(e) => setNss(e.target.value)}
+          required
+        />
+        <input
+          type="number"
+          placeholder="RFC"
+          value={rfc}
+          onChange={(e) => setRfc(e.target.value)}
           required
         />
         <button type="submit">Guardar</button>
